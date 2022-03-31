4399904b956747e8e8fce49f0a56aa45d9a32eab
Nathan PROVOST <nathan.provost78990@gmail.com>
2022-03-31 14:47:13 +0200

    auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,20 +1,16 @@
-const jwt = require('jsonwebtoken');
+const { signJwt, verifyJwt } = require('../utils/jwt');
 
 module.exports = (req, res, next) => {
     try {
         const user = req.session.user;
-        const decodedtoken = jwt.verify(req.session.token, 'RANDOM_TOKEN_SECRET');
+        const decodedtoken = verifyJwt(req.session.token);
         const uuid = decodedtoken.uuid;
         if (user.uuid && user.uuid !== uuid) {
             throw 'user ID non valable';
         }
 
         session = req.session;
-        session.token = jwt.sign(
-            { uuid: user.uuid, remember: decodedtoken.remember ?? false }, 
-            'RANDOM_TOKEN_SECRET', 
-            { expiresIn: false === decodedtoken.remember ? '10s' : '24h' }
-        );
+        session.token = signJwt(user.uuid, decodedtoken.remember)
 
         next();
     } catch (error) {
